c6eb9da3311485e28653b6ec0d42306a950d4c64
KilHyun-Kim <rlaarlfgus@gmail.com>
2021-08-08 14:36:15 +0900

    Updates

--- src/components/layout/Contents.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-const Contents = styled.div`
-  margin-left: 24px;
-  margin-right: 24px;
-`;
-
-export default Contents;
--- src/styles/palette.js
+++
@@ -1,6 +0,0 @@
-const palette = {
-  orange: ['#FF650F'],
-  gray: ['#C4C4C4', '#616161'],
-};
-
-export default palette;
